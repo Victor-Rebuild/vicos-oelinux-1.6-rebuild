f530edeb09bbf93b8db11b61ae61f7228af8b753
Switch-modder <deolrajjyot@gmail.com>
2025-08-17 21:31:49 -0400

    Run inject-anki with the env variable instead of just exporting it

--- build/docker-ota-build.sh
+++ build/docker-ota-build.sh
@@ -166,9 +166,7 @@ docker run ${BUILD_ARGS_TERM} \
     -v "$(pwd)/build/cache/ccache:/home/build/.ccache" \
     victor-oe-builder bash -c "cd ~/vicos-oelinux/poky && rm -rf build/tmp-glibc/deploy/images/apq8009-robot-robot-perf/* && source build/conf/set_bb_env.sh && export ANKI_BUILD_VERSION=$INCREMENT && source ~/vicos-oelinux/poky/build/conf/set_anki_build_version.sh && setenv-8009-robot-image$PERForUSER && bitbake -c cleanall victor anki-version rampost base-files wired iptables machine-robot-image core-image-anki-initramfs update-engine systemd ankiinit initscripts initscript-anki system-conf && $ToDo && export OTA_MANIFEST_SIGNING_KEY=$OTA_SIGNING_PASSWORD && ANKIDEV=$ADEV DO_SIGN=$DO_SIGN make && exit 0"
 
-export RUN_FROM_MAIN=1
-
-./build/inject-anki.sh
+RUN_FROM_MAIN=1 ./build/inject-anki.sh
 
 echo
 echo -e "\033[1;32mComplete. Your $PRODorOSKR OTA is in $(pwd)/_build/$OTA_NAME.\033[0m"
