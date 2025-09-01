08f716f786648923d018671c40c39f963bccbab9
Switch-modder <deolrajjyot@gmail.com>
2025-09-01 11:23:46 -0400

    A bit of cleanup

--- poky/meta-rebuild/recipes/update-unengine/files/rebuild-update-unengine.sh
+++ poky/meta-rebuild/recipes/update-unengine/files/rebuild-update-unengine.sh
@@ -4,34 +4,29 @@
 
 logger -t jank-auto-updater "Starting 1.6-rebuild auto update service"
 
-echo "Is there a pending update?"
-logger -t jank-auto-updater "Is there a pending update?"
+
 if [ -f /run/rebuild-updated ]; then
-    echo "An update is already in progress, waiting until nightly reboot"
-    logger -t jank-auto-updater "An update is already in progress, waiting until nightly reboot"
-    exit 1
+    echo "An update is already pending, waiting until nightly reboot"
+    logger -t jank-auto-updater "An update is already pending, waiting until nightly reboot"
+    exit 0
 else
     echo "No updates pending, continuing..."
     logger -t jank-auto-updater "No updates pending, continuing..."
 fi
 
-echo "Are auto updates enabled?"
-logger -t jank-auto-updater "Are auto updates enabled?"
 if [ -f /data/data/user-do-not-auto-update ]; then
     echo "Auto update disabled by user, falling out"
     logger -t jank-auto-updater "Auto update disabled by user, falling out"
-    exit 1
+    exit 0
 else
     echo "Auto updates are not disabled, continuing with updating"
     logger -t jank-auto-updater "Auto updates are not disabled, continuing with updating"
 fi
 
-echo "Has this build had dev done on it?"
-logger -t jank-auto-updater "Has this build had dev done on it?"
 if [ -f /anki-devtools ]; then
     echo "Build has been deployed to, not auto updating"
     logger -t jank-auto-updater "Build has been deployed to, not auto updating"
-    exit 1
+    exit 0
 else
     echo "Build is stock, auto updates allowed"
     logger -t jank-auto-updater "Build is stock, auto updates allowed"
@@ -43,12 +38,10 @@ BUILDINF="$(cat /build.prop)"
 CMDLINE="$(cat /proc/cmdline)"
 CURRENT_VERSION=$(getprop ro.anki.version)
 DEV_BUILD_ID=d
+EXTENSION=$(getprop ro.build.id)
 OSKR_BUILD_ID=oskr
 REBUILD_URL="http://modder.my.to:81/otas/1.6-rebuild"
-EXTENSION=$(getprop ro.build.id)
 
-echo "Is this an indev or release ota?"
-logger -t jank-auto-updater "Is this an indev or release ota?"
 if [ -f /etc/rebuild-indev ]; then
     echo "Indev ota detected, downloading from indev stack"
     logger -t jank-auto-updater "Indev ota detected, downloading from indev stack"
@@ -77,18 +70,13 @@ else
     logger -t jank-auto-updater "Current slot is a, update will install to b."
 fi
 
-echo "Getting current firmware version"
-logger -t jank-auto-updater "Getting current firmware version"
-echo "Current version: $CURRENT_VERSION"
-logger -t jank-auto-updater "Current version: $CURRENT_VERSION"
 
-echo "Getting update version"
-logger -t jank-auto-updater "Getting update version"
+echo "Current firmware version: $CURRENT_VERSION"
+logger -t jank-auto-updater "Current firmware version: $CURRENT_VERSION"
+
 echo "Target update version is $TARGET_VERSION"
 logger -t jank-auto-updater "Target update version is $TARGET_VERSION"
 
-echo "OSKR, Dev, or Prod?"
-logger -t jank-auto-updater "OSKR, Dev, or Prod?"
 if [[ ${EXTENSION} == $CURRENT_VERSION$DEV_BUILD_ID ]]; then
 	echo "Build type is dev"
     logger -t jank-auto-updater "Build type is dev"
@@ -153,6 +141,7 @@ elif [[ ${PROD} = 1 ]]; then
 fi
 
 sync
+echo
 echo "Update Done"
 logger -t jank-auto-updater "Update Done"
 exit 0
